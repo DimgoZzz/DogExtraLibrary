91628eb0efa10463b4acef630c515046d4149fc5
Dimgo <dmgolovinzzz@gmail.com>
2022-11-26 20:23:50 +0300

    work: migrated some test funtions

---
+++ Include/DSLTests/Math/Int/IntMathCTests.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "DSL/Math/Int/IntMathC.h"
+
+namespace dsl::math_c::tests
+{
+	void absPerfTest(Int32 loopCount, Int32 arraySize);
+}
---
+++ Include/DSLTests/Math/Int/IntMathTests.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "DSL/Math/Int/IntMath.h"
+
+#include "DSLTests/Math/Int/IntMathCTests.h"
+#include "DSLTests/Math/Int/IntMathCustomTests.h"

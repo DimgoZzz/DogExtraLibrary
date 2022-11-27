8d428280685386b881250c6914bd8075bdecad4d
Dimgo <dmgolovinzzz@gmail.com>
2022-11-27 16:14:05 +0300

    work: done abs test

--- Include/DSLTests/Math/Int/IntMathCustomTests.h
+++ Include/DSLTests/Math/Int/IntMathCustomTests.h
@@ -1,2 +1,63 @@
 #pragma once
 #include "DSL/Math/Int/IntMathCustom.h"
+#include "DSL/Time/Stopwatch.h"
+
+#include <vector>
+#include <string>
+#include <typeinfo>
+#include <sstream>
+
+
+namespace dsl::math_custom::tests
+{
+	template<class T>
+	std::string absPerfTest(Int32 loopCount = 1000, Int32 arraySize = 1000)
+	{
+		std::vector<dsl::Int64> measureValues(loopCount);
+		dsl::Stopwatch sw;
+
+
+		for (int loop = 0; loop < loopCount; ++loop)
+		{
+			//Init array for calculations
+			std::vector<T> testV(arraySize);
+
+			for (dsl::Int32 i = 0; i < arraySize; ++i)
+			{
+				testV[i] = -static_cast<T>(i);
+			}
+			//End Init data for calculations
+
+			sw.start();
+
+			for (dsl::Int32 i = 0; i < arraySize; ++i)
+			{
+				testV[i] = dsl::math_custom::integer::abs(testV[i]);
+			}
+
+			sw.stop();
+
+			measureValues.push_back(sw.getTicksElapsed());
+
+			sw.reset();
+		}
+
+		//Get result
+		dsl::Double avarage = 0;
+		for (auto i : measureValues)
+		{
+			avarage += i;
+		}
+		avarage = avarage / static_cast<dsl::Double>(loopCount);
+
+		std::stringstream resultText;
+		resultText << "[math_custom]absPerfTest<" << typeid(T).name() 
+		<< ">\t(loopCount " << loopCount << " | arraySize " << arraySize <<")" << std::endl;
+
+		resultText << "Avarage time in ticks : " << avarage <<
+			" (" << dsl::Stopwatch::Tick::toMilliseconds(static_cast<Int64>(avarage))<< "ms)" << std::endl;
+		resultText << std::endl;
+
+		return resultText.str();
+	}
+}
